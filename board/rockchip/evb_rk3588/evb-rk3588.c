76c84cf7daeaaaed10e3b7865b1ba651851c37ca
Weizhao Ouyang <weizhao.ouyang@arm.com>
2023-12-21 15:40:31 +0800

    Bring up RK3588 SystemReady
    Signed-off-by: Weizhao Ouyang <weizhao.ouyang@arm.com>

--- board/rockchip/evb_rk3588/evb-rk3588.c
+++ board/rockchip/evb_rk3588/evb-rk3588.c
@@ -3,8 +3,31 @@
  * Copyright (c) 2023 Rockchip Electronics Co,. Ltd.
  */
 
+#include <efi_loader.h>
 #include <fdtdec.h>
 #include <fdt_support.h>
+
+#define ROCKCHIP_UPDATABLE_IMAGES 1
+
+#if IS_ENABLED(CONFIG_EFI_HAVE_CAPSULE_SUPPORT)
+struct efi_fw_image fw_images[ROCKCHIP_UPDATABLE_IMAGES] = {0};
+
+struct efi_capsule_update_info update_info = {
+	.dfu_string = "mmc 0=u-boot.itb raw 0x4000 0x2000",
+	.num_images = ROCKCHIP_UPDATABLE_IMAGES,
+	.images = fw_images,
+};
+
+void rockchip_capsule_update_board_setup(void)
+{
+	efi_guid_t uboot_image_type_guid = ROCKCHIP_UBOOT_IMAGE_GUID;
+
+	guidcpy(&fw_images[0].image_type_id, &uboot_image_type_guid);
+	fw_images[0].fw_name = u"ROCKCHIP_UBOOT";
+
+	env_set("boot_targets", "usb");
+}
+#endif /* EFI_HAVE_CAPSULE_SUPPORT */
 
 #ifdef CONFIG_OF_BOARD_SETUP
 static int rk3588_add_reserved_memory_fdt_nodes(void *new_blob)
