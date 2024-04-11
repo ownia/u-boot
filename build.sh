7c533e13f5b09a26a8232c1279c54aa4cf22e37b
Weizhao Ouyang <weizhao.ouyang@arm.com>
2024-04-11 11:10:41 +0800

    Update build.sh
    Signed-off-by: Weizhao Ouyang <weizhao.ouyang@arm.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-export ROCKCHIP_TPL=../rkbin/bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.12.bin 
-export BL31=../rkbin/bin/rk35/rk3588_bl31_v1.40.elf
-export TEE=../rkbin/bin/rk35/rk3588_bl32_v1.13.bin
+export ROCKCHIP_TPL=../../rk3588/rkbin/bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.12.bin
+export BL31=../../rk3588/rkbin/bin/rk35/rk3588_bl31_v1.40.elf
+export TEE=../../rk3588/rkbin/bin/rk35/rk3588_bl32_v1.13.bin
 make CROSS_COMPILE=aarch64-linux-gnu- armsom-rk3588_defconfig
 make CROSS_COMPILE=aarch64-linux-gnu- -j4
 
