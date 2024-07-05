d753006e2558c3a141c3f5f385909177fc5d4b7d
Weizhao Ouyang <weizhao.ouyang@arm.com>
2024-07-05 16:08:18 +0800

    rockchip: toybrick_rk3588: SystemReady IR updates
    Signed-off-by: Weizhao Ouyang <weizhao.ouyang@arm.com>

--- include/configs/toybrick_rk3588.h
+++ include/configs/toybrick_rk3588.h
@@ -8,6 +8,10 @@
 
 #include <configs/rk3588_common.h>
 
+#define ROCKCHIP_UBOOT_IMAGE_GUID \
+	EFI_GUID(0x5452a04a, 0x462d, 0x4433, 0x97, 0x78, \
+		 0x2b, 0x52, 0x59, 0x27, 0x7b, 0xb3)
+
 #define ROCKCHIP_DEVICE_SETTINGS \
 		"stdout=serial,vidconsole\0" \
 		"stderr=serial,vidconsole\0"
