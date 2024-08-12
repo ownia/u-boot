7a09841f3780fe8d6998e6f344c7002e4bac20bc
Weizhao Ouyang <weizhao.ouyang@arm.com>
2024-08-12 15:34:24 +0800

    board: armsom sige7 SystemReady support
    Signed-off-by: Weizhao Ouyang <weizhao.ouyang@arm.com>

--- build.sh
+++ build.sh
@@ -3,8 +3,17 @@ export BL31=../trusted-firmware-a/build/rk3588/release/bl31/bl31.elf
 #export BL31=../rkbin/bin/rk35/rk3588_bl31_v1.40.elf
 export TEE=../rkbin/bin/rk35/rk3588_bl32_v1.13.bin
 
-make CROSS_COMPILE=aarch64-linux-gnu- toybrick-rk3588_defconfig
-make CROSS_COMPILE=aarch64-linux-gnu- -j4
+if [ "$1" == "toybrick" ];
+then
+	make CROSS_COMPILE=aarch64-linux-gnu- toybrick-rk3588_defconfig
+	make CROSS_COMPILE=aarch64-linux-gnu- -j16
+fi
+
+if [ "$1" == "armsom" ];
+then
+	make CROSS_COMPILE=aarch64-linux-gnu- sige7-rk3588_defconfig
+	make CROSS_COMPILE=aarch64-linux-gnu- -j16
+fi
 
 # Note:
 # update CRT files
